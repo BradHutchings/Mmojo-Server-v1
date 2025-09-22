ff4b1474e8eb06cc0567d3f6078d2918d670b9fd
Brad Hutchings <brad@componentx.com>
2025-09-22 09:07:43 -0700

    Update common.h
    Signed-off-by: Brad Hutchings <brad@componentx.com>

--- common/common.h
+++ common/common.h
@@ -278,7 +278,8 @@ struct common_params {
     int32_t n_ctx                 =  4096; // context size
     int32_t n_batch               =  2048; // logical batch size for prompt processing (must be >=32 to use BLAS)
     // mmojo-server START
-    int32_t n_batch_sleep_ms      =     0; // delay in milliseconds after processing each batch.
+    int32_t n_batch_sleep_ms          =     0; // delay in milliseconds after processing each batch.
+    std:string default_ui_endpoint    =    ""; // endpoint for chat UI
     // mmojo-server END
     int32_t n_ubatch              =   512; // physical batch size for prompt processing (must be >=32 to use BLAS)
     int32_t n_keep                =     0; // number of tokens to keep from initial prompt
