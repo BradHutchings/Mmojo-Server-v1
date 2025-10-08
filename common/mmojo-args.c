c5eabb69ae5442a40dbe73190c8f2feb09048c20
Brad Hutchings <brad@componentx.com>
2025-10-08 11:37:07 -0700

    Update mmojo-args.c
    Signed-off-by: Brad Hutchings <brad@componentx.com>

--- common/mmojo-args.c
+++ common/mmojo-args.c
@@ -34,11 +34,12 @@
 
 // __static_yoink("zipos");
 
-alignas(int8_t) const int8_t kHexToInt[256] = {
+// alignas(int8_t) 
+const int8_t kHexToInt[256] = {
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  // 0x00
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  // 0x10
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  // 0x20
-    0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  -1, -1, -1, -1, -1, -1,  // 0x30
+     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, -1, -1, -1, -1, -1, -1,  // 0x30
     -1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1,  // 0x40
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  // 0x50
     -1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1,  // 0x60
@@ -570,13 +571,14 @@ int mmojo_args(const char *path, char ***argv) {
           APPEND(arg, '$');
           break;
         case DOLLAR_VAR:
-        case DQUOTE_DOLLAR_VAR:
+        case DQUOTE_DOLLAR_VAR: {
           char *val = getenv(var_ptr);
           if (!val)
             val = "";
           while (*val)
             APPEND(arg, *val++);
           break;
+        }
         case DOLLAR_LCB:
         case DQUOTE_DOLLAR_LCB:
           APPEND(arg, '$');
