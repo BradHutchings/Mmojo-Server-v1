236174a8c26fc3f567117429465fa302422ff8bd
Brad Hutchings <brad@componentx.com>
2025-10-09 09:38:33 -0700

    Update customize-web-ui.sh
    Signed-off-by: Brad Hutchings <brad@componentx.com>

--- scripts-mmojo/customize-web-ui.sh
+++ scripts-mmojo/customize-web-ui.sh
@@ -3,6 +3,9 @@
 # This script rebuilds the chat web UI so that it can be customized and used with mmojo-server.
 # -Brad
 
+# Todo: Set $TODAY locally in this script. In the build process, duplicate the completion-ui directory as completion-ui-original.
+# Copy that directory to local share. Remove the replacement of $TODAY. -Brad 2025-10-09
+
 APP_NAME='Mmojo Chat'
 sed -i -e "s/>llama.cpp<\/h1>/>$APP_NAME<\/h1>/g" tools/server/webui/src/lib/components/app/chat/ChatScreen/ChatScreen.svelte
 sed -i -e "s/>llama.cpp<\/h1>/>$APP_NAME<\/h1>/g" tools/server/webui/src/lib/components/app/chat/ChatSidebar/ChatSidebar.svelte
